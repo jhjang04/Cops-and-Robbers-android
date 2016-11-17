59ca65bbd133fd0f650dff4b695f9f8bc04edf53
ByungjaeMun <mbj9424@gmail.com>
2016-11-17 11:50:43 +0900

    First Activity UI and Create Second Activity

--- app/src/main/java/car/adroid/com/MainActivity.java
+++ app/src/main/java/car/adroid/com/MainActivity.java
@@ -2,8 +2,12 @@ package car.adroid.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnCreate, btnJoin;
+    private TextView txtRoomNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
