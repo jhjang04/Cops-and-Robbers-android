ba4ee8860a67d1eea835d28ad50f422ed92ac1aa
ByungjaeMun <mbj9424@gmail.com>
2016-11-17 13:05:15 +0900

    Second Activty UI and create third activity

--- app/src/main/java/car/adroid/com/MainActivity.java
+++ app/src/main/java/car/adroid/com/MainActivity.java
@@ -1,17 +1,63 @@
 package car.adroid.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCreate, btnJoin;
-    private TextView txtRoomNumber;
+    private EditText txtRoomNumber,txtName;
+    private String roomNum;
+
+    private Boolean IsRoomNumber(){
+        if ( txtRoomNumber.getText().toString().length() == 0 ) {
+            // 아닐경우에 대한 적절한 처리
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void GetRoomNumber(){
+        roomNum = txtRoomNumber.getText().toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtName = (EditText)findViewById(R.id.txtName);
+        txtRoomNumber = (EditText)findViewById(R.id.txtRoomNum);
+        btnCreate = (Button)findViewById(R.id.btnCreate);
+        btnJoin = (Button)findViewById(R.id.btnJoin);
+
+        btnCreate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(IsRoomNumber())
+                    GetRoomNumber();
+
+                Intent intent = new Intent(MainActivity.this, TeamActivity.class);
+                // intent에 Name을 담아 전달
+                startActivity(intent);
+        }
+        });
+
+        btnJoin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(IsRoomNumber())
+                    GetRoomNumber();
+
+                Intent intent = new Intent(MainActivity.this, TeamActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/car/adroid/com/TeamActivity.java
+++ app/src/main/java/car/adroid/com/TeamActivity.java
@@ -2,12 +2,54 @@ package car.adroid.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class TeamActivity extends AppCompatActivity {
+
+    private Button btnCop, btnRobber,btnStart;
+    private ListView listCop, listRobber;
+
+    private Boolean IsListFocused(){
+        if(listCop.isFocused())
+            return true;
+        else
+            return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+
+        btnCop = (Button)findViewById(R.id.btnCop);
+        btnRobber = (Button)findViewById(R.id.btnRobber);
+        btnStart = (Button)findViewById(R.id.btnStart);
+        listCop = (ListView)findViewById(R.id.listCop);
+        listRobber = (ListView)findViewById(R.id.listRobber);
+
+        btnCop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //if(IsListFocused())
+                // 리스트의 아이템 이동
+            }
+        });
+
+        btnRobber.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //if(IsListFocused())
+                // 리스트의 아이템 이동
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
