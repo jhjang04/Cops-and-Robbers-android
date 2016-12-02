d25220532205d4d92cd2b5e5e885d3f612332a07
ByungjaeMun <mbj9424@gmail.com>
2016-12-02 20:42:30 +0900

    chatting UI

--- app/src/main/java/car/adroid/com/TeamChat.java
+++ app/src/main/java/car/adroid/com/TeamChat.java
@@ -1,14 +1,111 @@
 package car.adroid.com;
 
+import android.content.Intent;
+import android.database.DataSetObserver;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class TeamChat extends FragmentActivity {
+
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private Button buttonSend;
+
+    Intent intent;
+    private boolean side = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_chat);
+
+
+        buttonSend = (Button) findViewById(R.id.btnTeamChat);
+        listView = (ListView) findViewById(R.id.lvTeamChat);
+        chatText = (EditText) findViewById(R.id.tvTeamMessage);
+
+        // hide keyboard when touching background
+        listView.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return false;
+            }
+        });
+
+        // hide keyboard when touching background
+        findViewById(R.id.bgTeamChat).setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return false;
+            }
+        });
+
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.activity_chat_singlemessage);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage();
+                }
+                return false;
+            }
+        });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                sendChatMessage();
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+    }
+
+    private boolean IsEmptyText(){
+        if(chatText.getText().toString().isEmpty() || !chatText.getText().toString().matches(".*\\w.*") )
+        {
+            chatText.setText("");
+            return true;
+        }
+
+
+        return false;
+    }
+
+    private boolean sendChatMessage(){
+        if(!IsEmptyText())
+        {
+            chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+            chatText.setText("");
+            side = !side;
+            return true;
+        }
+
+        return false;
     }
 }
