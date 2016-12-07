f1632cac208c95eef3be722832008a7ab760c59d
jhjang04 <jjh920415@naver.com>
2016-12-07 21:00:18 +0900

    make logger as a service

--- app/src/main/java/car/adroid/util/SimpleLogger.java
+++ app/src/main/java/car/adroid/util/SimpleLogger.java
@@ -1,6 +1,11 @@
 package car.adroid.util;
 
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,11 +17,15 @@ import car.adroid.config.AppConfig;
  * Created by jjh on 2016-11-25.
  */
 
-public class SimpleLogger {
+public class SimpleLogger extends Service {
 
     private static StringBuffer mBuffer = new StringBuffer();
     private static Date mLastShow = new Date();
     private static final String TAG = "SimpleLogger";
+
+    private Context mContext = this;
+    private Handler mHandler = null;
+    private Runnable mRunnable = null;
 
 
     public static void debug(Context context , String txt) {
@@ -36,21 +45,56 @@ public class SimpleLogger {
             return;
         }
 
-        Date curDate = new Date();
-        if(curDate == null){
-            curDate = new Date();
-        }
+//        Date curDate = new Date();
+//        if(mLastShow == null){
+//            mLastShow= new Date();
+//        }
         if(mBuffer == null){
             mBuffer = new StringBuffer();
         }
-        if((curDate.getTime() -  mLastShow.getTime()) > 2000) {
-            Toast.makeText(context , mBuffer.toString() , Toast.LENGTH_SHORT).show();
-            mLastShow = curDate;
-            mBuffer.setLength(0);
+        mBuffer.append(txt).append("\n");
+
+//        if((curDate.getTime() -  mLastShow.getTime()) > AppConfig.LOGGER_SHOW_DELAY) {
+//            Toast.makeText(context , mBuffer.toString() , Toast.LENGTH_LONG).show();
+//            mLastShow = curDate;
+//            mBuffer.setLength(0);
+//        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(!AppConfig.DEBUG){
+            stopService(new Intent(mContext , SimpleLogger.class));
+            return;
         }
-        else {
-            mBuffer.append(txt).append("\n");
-        }
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            public void run() {
+                mHandler.postDelayed(mRunnable, AppConfig.LOGGER_SHOW_DELAY);
+//                if((curDate.getTime() -  mLastShow.getTime()) > AppConfig.LOGGER_SHOW_DELAY) {
+                if(mBuffer == null || mBuffer.length() == 0){
+//                    return;
+                }
+                Toast.makeText(mContext , mBuffer.toString() , Toast.LENGTH_LONG).show();
+//                mLastShow = curDate;
+                mBuffer.setLength(0);
+            }
+        };
+        mHandler.post(mRunnable);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        mHandler.removeCallbacks(mRunnable);
+        super.onDestroy();
     }
 
 }
